6923398e09b3d5ed18756d6b638a23e5adc19367
suidonx <neesama178@gmail.com>
2026-02-21 06:33:23 +0900

    Todo Listの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,195 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import { useState } from "react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Input,
+  List,
+  Text,
+} from "@chakra-ui/react";
+
+type TodoItem = {
+  id: string;
+  name: string;
+  isDone: boolean;
+  isEdit: boolean;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  // state
+  const [newTodoItem, setNewTodoItem] = useState("");
+  const [todoList, setTodoList] = useState<TodoItem[]>([]);
+  const [updateTodo, setUpdateTodo] = useState("");
+
+  // Todoに追加したタスクを条件ごとに集計する関数
+  const countTask = () => {
+    return {
+      total: todoList.length,
+      done: todoList.filter((todo) => todo.isDone).length,
+      todo: todoList.filter((todo) => !todo.isDone).length,
+    };
+  };
+  const status = countTask();
+
+  // Create
+  // オブジェクトを生成してTodo Listに追加する
+  const onClickAdd = () => {
+    const uuid = self.crypto.randomUUID();
+
+    const addTodoItem = {
+      id: uuid,
+      name: newTodoItem,
+      isDone: false,
+      isEdit: false,
+    };
+    const copiedTodoList = [...todoList, addTodoItem];
+
+    setTodoList(copiedTodoList);
+    setNewTodoItem("");
+  };
+
+  // Read
+  // チェックボックスをクリックした時、タスクが完了状態かどうかを判定する
+  // 現在の状態と反対の状態に更新する
+  const onCheckedChangeIsDone = (id: string) => {
+    const copiedTodoList = [...todoList];
+    const newTodoList = copiedTodoList.map((todo) => {
+      if (todo.id === id) {
+        const isDoneTodo = todo.isDone;
+        todo.isDone = !isDoneTodo;
+      }
+      return todo;
+    });
+
+    setTodoList(newTodoList);
+  };
+
+  // Update
+  // 編集完了後に保存ボタンをクリックしたとき、Todo Listを更新する
+  const onClickUpdate = (id: string) => {
+    const copiedTodoList = [...todoList];
+    const newTodoList = copiedTodoList.map((todo) => {
+      if (todo.id === id) {
+        todo.name = updateTodo;
+        todo.isEdit = false;
+      }
+      return todo;
+    });
+    setTodoList(newTodoList);
+  };
+
+  // 編集ボタンをクリックした時、そのTodoの編集フラグをtrueにする
+  const onClickEdit = (id: string) => {
+    const copiedTodoList = [...todoList];
+    const newTodoList = copiedTodoList.map((todo) => {
+      if (todo.id === id) {
+        todo.isEdit = true;
+        setUpdateTodo(todo.name); // 初期値をupdate用のstateに保存
+      }
+      return todo;
+    });
+    setTodoList(newTodoList);
+  };
+
+  // Delete
+  // 削除ボタンをクリックしたとき、該当のTodoを削除する
+  const onClickDelete = (id: string) => {
+    if (confirm("本当によろしいですか？")) {
+      const copiedTodoList = [...todoList];
+      const newTodoList = copiedTodoList.filter((todo) => todo.id !== id);
+      setTodoList(newTodoList);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Box mb={3}>
+        <Heading size="2xl">Todo List</Heading>
+        <Text fontSize="md">タスクを管理することができます。</Text>
+      </Box>
 
-export default App
+      <Box mb={3}>
+        <Heading size="lg">タスク集計</Heading>
+        <Text>全てのタスク: {status.total}</Text>
+        <Text>完了済み: {status.done}</Text>
+        <Text>未完了: {status.todo}</Text>
+      </Box>
+
+      <Box mb={3}>
+        <Heading size="lg" mb="2">
+          新規タスクを追加する
+        </Heading>
+        <Flex>
+          <Input
+            value={newTodoItem}
+            onChange={(e) => setNewTodoItem(e.target.value)}
+            mr={5}
+          />
+          <Button
+            onClick={onClickAdd}
+            disabled={newTodoItem === ""}
+            colorPalette="cyan"
+          >
+            保存
+          </Button>
+        </Flex>
+      </Box>
+
+      <Box>
+        <Heading size="lg">ToDo一覧</Heading>
+        <List.Root listStyle="none">
+          {todoList.map((item) => {
+            // 編集フラグがtrueの時は編集用のフォームを表示
+            return item.isEdit ? (
+              <List.Item key={item.id} mb={5}>
+                <Flex alignItems="center">
+                  <Input
+                    defaultValue={item.name}
+                    onChange={(e) => setUpdateTodo(e.target.value)}
+                    mr={5}
+                  />
+                  <Button
+                    onClick={() => onClickUpdate(item.id)}
+                    colorPalette="teal"
+                  >
+                    保存
+                  </Button>
+                </Flex>
+              </List.Item>
+            ) : (
+              // 編集フラグがfalseの時は通常のTodoを表示
+              <List.Item key={item.id} mb={3}>
+                <Flex alignItems="center" gap={4}>
+                  <Checkbox.Root
+                    onCheckedChange={() => onCheckedChangeIsDone(item.id)}
+                  >
+                    <Checkbox.Control />
+                    <Checkbox.HiddenInput />
+                    <Checkbox.Label>{item.name}</Checkbox.Label>
+                  </Checkbox.Root>
+                  <Button
+                    onClick={() => onClickEdit(item.id)}
+                    colorPalette="yellow"
+                  >
+                    編集
+                  </Button>
+                  <Button
+                    onClick={() => onClickDelete(item.id)}
+                    colorPalette="red"
+                  >
+                    削除
+                  </Button>
+                </Flex>
+              </List.Item>
+            );
+          })}
+        </List.Root>
+      </Box>
+    </>
+  );
+}
+export default App;
